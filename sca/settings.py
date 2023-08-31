396f6c5f11e7989fb2d612af138b0249736618d9
JEAN DE SOUZA MORAIS <you@example.com>
2023-08-31 19:22:59 -0300

    novos

--- sca/settings.py
+++ sca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mzy3(#99m8438$7!vqv#)=ki-5exhpimq561y2fzue#179=qt_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'sca.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
